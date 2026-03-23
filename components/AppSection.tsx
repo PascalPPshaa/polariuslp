7be70f658e5fd44e72e496e36b95a8612e0d0e5b
PascalPPshaa <pascalpahlevi9@gmail.com>
2026-03-23 13:08:24 +0700

    p2

--- components/AppSection.tsx
+++ components/AppSection.tsx
@@ -25,7 +25,7 @@ export default function AppSection() {
             <h2 className="text-4xl lg:text-5xl font-extrabold text-white leading-tight mb-6">
               Segera Konsultasi Bersama Kami dan Nikmati Pelayanan yang Lebih Cepat!
             </h2>
-
+            {/* eslint-disable-next-line react/no-unescaped-entities */}
             <p className="text-white/80 leading-relaxed mb-8 max-w-md">
               Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
             </p>
