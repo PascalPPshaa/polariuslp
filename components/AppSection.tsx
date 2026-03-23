4035bc6c6adbfb5bcb82116cfcccf7599c583630
PascalPPshaa <pascalpahlevi9@gmail.com>
2026-03-23 13:02:39 +0700

    penghapusan (') pada text

--- components/AppSection.tsx
+++ components/AppSection.tsx
@@ -27,7 +27,7 @@ export default function AppSection() {
             </h2>
 
             <p className="text-white/80 leading-relaxed mb-8 max-w-md">
-              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry &apos;s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
             </p>
 
             <p className="text-white font-semibold flex items-center gap-2 mb-5">
